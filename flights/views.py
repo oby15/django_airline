95a530db60e4f0622bd5615cba0c500e7bad25e4
Oliver Brown <olliebrown3@hotmail.co.uk>
2022-02-24 20:08:40 +0000

    passenger model added

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Flight, Airport, Passenger
 
 # Create your views here.
+
+def index(request):
+    return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = Passenger.objects.all()
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers
+        })
